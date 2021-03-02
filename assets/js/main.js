7569314f51103e3a3697158a152dca57c012561b
md-ataur <mdataur15@gmail.com>
2021-03-03 00:00:39 +0600

    Portfolio section updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ jQuery(document).ready(function($) {
         // how many entries do you want?
         // default: 4
         // valid values: any integer
-        limit: 4,
+        limit: 5,
 
         // want to offset results being displayed?
         // default: false
