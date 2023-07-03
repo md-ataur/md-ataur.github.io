34c5ff87607e85a1da3dedf5c6b3ef88b073401a
md-ataur <mdataur15@gmail.com>
2023-07-03 12:55:57 +0600

    bootstrap updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,26 @@
 jQuery(document).ready(function ($) {
 
-
     /*======= Skillset *=======*/
 
     $('.level-bar-inner').css('width', '0');
 
     $(window).on('load', function () {
-
         $('.level-bar-inner').each(function () {
-
             var itemWidth = $(this).data('level');
-
             $(this).animate({
                 width: itemWidth
             }, 800);
-
         });
-
     });
 
     /* jQuery RSS - https://github.com/enginkizil/FeedEk */
 
     $('#divFeed').FeedEk({
         FeedUrl: 'https://techtalksbd.wordpress.com/feed',
-        MaxCount: 3,
+        MaxCount: 4,
         ShowDesc: true,
         ShowPubDate: false,
-        DescCharacterLimit: 100,
+        DescCharacterLimit: 120,
         TitleLinkTarget: '_blank',
         DateFormat: 'MM/dd/yyyy',
         DateFormatLang: 'en',
@@ -34,7 +28,7 @@ jQuery(document).ready(function ($) {
         ShowAuthor: false,
         AuthorLabel: "Author:",
         Success: function (feedItems) { },
-        Error: function (error) { }
+        Error: function (error) { console.log(error) }
     });
 
 });
